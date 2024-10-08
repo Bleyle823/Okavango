6e1ecb141e5eade9f9cdad02797091ed4f854640
Bleyle <Bennybleylle09@gmail.com>
2024-10-08 20:26:55 +0300

    Update LendingPool.sol

--- Contracts/contracts/LendingPool.sol
+++ Contracts/contracts/LendingPool.sol
@@ -111,6 +111,29 @@ contract LendingPool is AxelarExecutable, ReentrancyGuard, Ownable{
             Loan storage loan = loans[_loanId];
             require(loan.isActive, "Loan is not active");
             require(loan.borrower == msg.sender, "Not the borrower");
+
+            uint256 interest = calculateInterest(
+                loan.amount, 
+                loan.startTime, 
+                loan.duration, 
+               );
+               uint256 totalRepayment = loan.amount + interest;
+            
+            require(
+                lendingToken.transferFrom(
+                    address(this), 
+                    msg.sender, 
+                    totalRepayment
+                    ),
+                "Transfer failed"
+            );
+
+            rwaToken.transferFrom ( address(this),  msg.sender, loan.collateralId);
+                loan.isActive = false;
+
+                emit LoanRepaid(_loanId);
+
+
         }
 
         //Calculate Interest
@@ -131,10 +154,121 @@ contract LendingPool is AxelarExecutable, ReentrancyGuard, Ownable{
             //Calculated interested: (amount * rate * time) (365 days * 100 * 10^decimals)
             //for a 5% annual rate
 
-            uint256 interst = (_amount * 5 * timeElapsed) / (365 days * 100);
+            uint256 interest = (_amount * 5 * timeElapsed) / (365 days * 100);
 
             // Adjust for token decimals (assuming 6 decimals for USDC)
             return interest /1e6;
         }
 
+        //liquidate
+
+        function initiateCrossChain(
+            string memory destinationChain,
+            string memory destinationAddress,
+            uint256 _amount,
+            uint256 _collateralId,
+            uint256 _duration 
+            ) external payable  {
+            require(msg.value > 0, "gas payment is required");
+            require(
+                rwaToken.ownerOf(_collaeralId) == msg.sender;
+                "Not the owner of the RWA" 
+            )
+            uint256 collateralValue = rwaOracle.getRWAValue(_collateralId);
+            require(collateralValue * 100 / _amount >= LIQUIDATION_THRESHOLD, 
+            "Insufficient collateral" 
+            );
+            
+            bytes memory payload = abi.encode(
+                msg.sender,
+                _amount, 
+                _collateralId, 
+                _duration,
+
+            );
+
+            gasService.payNativeGasForContractCallWithToken{value: msg.value}(
+                destinationChain, 
+                destinationAddress, 
+                payload, 
+                lendingToken.symbol(),
+                _amount,
+                msg.sender
+                );
+            
+            rwaToken.transferFrom(msg.sender, address(this), _collateralId);
+            lendingToken.transferFrom(msg.sender, address(this), _amount);
+            lendingToken.approve(address(gateway), _amount);
+            
+            gateway.callContractWithToken(
+                destinationChain, 
+                destinationAddress, 
+                payload, 
+                lendingToken.symbol(), 
+                _amount
+            );
+            
+            // uint256 interest = calculateInterest(
+            //     _amount, 
+            //     block.timestamp, 
+            //     _duration, 
+            // );
+            // uint256 totalRepayment = _amount + interest;
+            // require(lendingToken.transferFrom(msg.sender, address(this), _collateralId), 
+            // "Transfer failed" 
+            // );
+            // rwaToken.transferFrom(address(this), msg.sender, _collateralId);
+            // emit CollateralReleased(_amount, _collateralId, msg.sender);
+            }
+  
+        function _executeWithToken(
+            string calldata _sourceChain,
+            string calldata _sourceAddress,
+            bytes calldata _payload, 
+            string calldata _tokenSymbol, 
+            uint256 
+        ) internal override {
+            require(
+                keccak256(bytes(tokenSymbol)) ==
+                   keccak256(bytes(lendingToken.symbol())),
+                   "invalid token"
+
+            );
+
+            sourceAddress = _sourceAddress;
+            sourceChain = _sourceChain;
+
+        (
+            address borrower,
+            uint256 _amount,
+            uint256 _collateralId, 
+            uint256 _duration,
+        ) = abi.decode(payload, (address, uint256, uint256, uint256));
+
+        //create the loan 
+
+        }
+        
+        function _createLoanInternal(
+            address borrower,
+            uint256 _amount,
+            uint256 _collateralId, 
+            uint256 _duration
+        )
+            internal 
+            returns(
+                uint256
+            )
+        {
+            uint256 loanId = nextLoanId++;
+            loans[loanId] = Loan({
+                amount: _amount,
+                collateralId: _collateralId,
+                startTime: block.timestamp,
+                duration: _duration,
+                borrower: borrower,
+                isActive: true
+            });
+            return loanId;
+        }
 }
